c080b76b98d88f4f85a48f7ac0831378a0d9149a
alexciotor <ciotor.alex@yahoo.com>
2020-10-24 14:10:02 +0300

    First Nav Project

---
+++ maine.js
@@ -0,0 +1,35 @@
+// Nav bar variable
+const nav =document.getElementById('nav');
+// Interactive logo variable
+const logo =document.getElementById('logos')
+
+//const list =document.getElementById('navlist')
+
+// list elements fron the nav bar
+const elements =document.querySelectorAll('.list')
+
+
+
+logo.addEventListener('click', function(){
+nav.classList.toggle('bar')
+ 
+ 
+
+})
+
+// JavaScript for hover interaction
+
+// elements.forEach(element=>{
+//     element.addEventListener('mouseenter', function(){
+//  element.style.backgroundColor ='rgb(170, 208, 243)'
+//     })
+// })
+
+
+//  elements.forEach(element=>{
+//     element.addEventListener('mouseleave', function(){
+//        element.style.backgroundColor='white'
+//     })
+// })
+
+ 
